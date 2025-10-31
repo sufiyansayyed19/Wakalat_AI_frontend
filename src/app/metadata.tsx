1a2e702c91c26eed9c3e0112067f8e4dec6583a7
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-11-01 00:13:48 +0530

    metadata.tsx added

---
+++ src/app/metadata.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "WAKALAT.AI",
+  description: "Your Legal AI Assistant",
+  icons: {
+    icon: '/white_logo.png',
+  },
+};
