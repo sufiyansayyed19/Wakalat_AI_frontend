253ffce2673eaf6a004bcb37ee1b37428f1641fa
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-11-01 00:12:19 +0530

    metadata.ts created

---
+++ src/app/metadata.ts
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "WAKALAT.AI",
+  description: "Your Legal AI Assistant",
+  icons: {
+    icon: [
+      {
+        url: "/white_logo.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/white_logo.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+    apple: {
+      url: "/white_logo.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  },
+};
